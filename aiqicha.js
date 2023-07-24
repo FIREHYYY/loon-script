3e0f714d7c518a7fa45add9236961cf53f77100b
FIREHYYY <54975995+FIREHYYY@users.noreply.github.com>
2023-07-24 10:57:54 +0800

    Update aiqicha.js

--- aiqicha.js
+++ aiqicha.js
@@ -1,3 +1,3 @@
 var body = $response.body
-    .replace(/\"svip\":\{\"status\":0,\"startTime\":\"\",\"endTime\":\"\"/, "\"svip\":\{\"status\":1,\"startTime\":\"\",\"endTime\":\"2999-12-31\"");
+    .replace(/\"svip\":\{\"status\":0,\"startTime\":\"\",\"endTime\":\"\"/, "\"svip\":\{\"status\":1,\"startTime\":\"\",\"endTime\":\"9999-12-31\"");
 $done({ body });
