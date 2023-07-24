342d4746fadbeb8b3a1dc9cdb2b01bcbb975504e
FIREHYYY <54975995+FIREHYYY@users.noreply.github.com>
2023-07-24 10:45:07 +0800

    Create aiqicha.js

---
+++ aiqicha.js
@@ -0,0 +1,3 @@
+var  body  =  $response . 身体
+    。替换( / \" svip \" : \{ \" status \" :0, \" startTime \" : \" \" , \" endTime \" : \ " \ " / ,  "\"svip \":\{\"status \":1,\"startTime \":\"\",\"endTime \":\"9999-12-31\"" ) ;
+$done ( { body } ) ;
